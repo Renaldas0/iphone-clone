9269e56ef76696e6fab86d548f297abbf44246e1
Renaldas Bendikas <97538312+Renaldas0@users.noreply.github.com>
2024-07-24 21:45:04 +0100

    resize iphone and begin texturing

--- my-project/src/components/ModelView.jsx
+++ my-project/src/components/ModelView.jsx
@@ -1,14 +1,16 @@
-import { PerspectiveCamera, View } from "@react-three/drei"
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import Lights from "./Lights"
 import { Suspense } from "react"
 import Iphone from "./Iphone"
+import { Html } from "@react-three/drei"
+import * as THREE from 'three'
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item }) => {
     return (
         <View
             index={index}
             id={gsapType}
-            className={`border-2 border-red-500 w-full h-full ${index === 2 ? "right-[-100%]" : ""}`}
+            className={`w-full h-full ${index === 2 ? "right-[-100%]" : ""}`}
         >
 
             {/* Ambient Light */}
@@ -18,12 +20,26 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
 
             <Lights />
 
-            <Suspense fallback={(<div>Loading...</div>)}>
-                <Iphone />
-            </Suspense>
+            <OrbitControls
+                makeDefault
+                ref={controlRef}
+                enableZoom={false}
+                enablePan={false}
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)}
+                onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())} />
+
+            <group ref={groupRef} name={`${index === 1 ? 'small' : 'large'}`} position={[0, 0, 0]}>
+                <Suspense fallback={<Html><div>Loading...</div></Html>}>
+                    <Iphone
+                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+                        item={item}
+                        size={size} />
+                </Suspense>
+            </group>
 
         </View>
     )
 }
 
-export default ModelView
+export default ModelView;
