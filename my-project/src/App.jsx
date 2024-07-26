c17225143d44620e9e03070cb17027796e79597f
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-07-26 20:00:15 +0100

    add footer

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -6,6 +6,7 @@ import Model from "./components/Model";
 import * as Sentry from '@sentry/react';
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
 
@@ -17,6 +18,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   )
 }
